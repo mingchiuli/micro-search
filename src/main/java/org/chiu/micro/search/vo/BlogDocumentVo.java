fa9c281a8a68fc757e4e6e302310c6c724392a9b
mingchiuli <80095372+mingchiuli@users.noreply.github.com>
2024-07-25 10:03:58 +0800

    Update BlogDocumentVo.java

--- src/main/java/org/chiu/micro/search/vo/BlogDocumentVo.java
+++ src/main/java/org/chiu/micro/search/vo/BlogDocumentVo.java
@@ -30,7 +30,7 @@ public class BlogDocumentVo {
 
     private String link;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime created;
 
     private Float score;
